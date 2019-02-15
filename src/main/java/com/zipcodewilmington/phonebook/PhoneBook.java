74b1ec8b704f68f3aad257cb88b0b5b639aa45a4
WilliamXu <williamxu@zipcoders-MacBook-Pro-90.local>
2019-02-15 08:58:52 -0500

    first part of lab complete

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,40 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.*;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+    TreeMap<String, String> directory = new TreeMap<String, String>();
+
+
+    public void add(String name, String phoneNumber) {
+        directory.put(name, phoneNumber);
+    }
+
+    public void remove(String name) {
+        directory.remove(name);
+    }
+
+    public String lookup(String name) {
+        String result = "";
+        if(directory.containsKey(name)){
+            result = directory.get(name);
+        }
+        return result;
+    }
+
+
+    public String reverseLookup(String phoneNumber) {
+        String returnKey = "";
+        Set<String> keys = directory.keySet();
+        for(String k : keys){
+            if (phoneNumber.equals(directory.get(k))) {
+                returnKey = k;
+            }
+        }
+        return returnKey;
+    }
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,72 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+    private PhoneBook phoneBook;
+    @Before
+    public void setup(){
+        phoneBook = new PhoneBook();
+    }
+
+    @Test
+    public void addTest(){
+        String name = "Will";
+        String phoneNumber = "302-293-1456";
+
+        phoneBook.add(name, phoneNumber);
+
+        String actual = phoneBook.lookup(name);
+        Assert.assertEquals(phoneNumber, actual);
+    }
+
+    @Test
+    public void removeTest(){
+        String name = "Will";
+        String phoneNumber = "302-293-1456";
+        String name2 = "Bob";
+        String phoneNumber2 = "302-293-3575";
+
+        phoneBook.add(name, phoneNumber);
+        phoneBook.add(name2, phoneNumber2);
+
+        phoneBook.remove(name);
+
+        String expected = "";
+        String actual = phoneBook.lookup(name);
+        String actual2 = phoneBook.lookup(name2);
+        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(phoneNumber2, actual2);
+    }
+
+
+    @Test
+    public void lookupTest(){
+        String name = "Will";
+        String phoneNumber = "302-293-1456";
+
+        phoneBook.add(name, phoneNumber);
+        String falseName = "Bob";
+        String expected = "";
+        String actual = phoneBook.lookup(falseName);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void reverseLookupTest(){
+        String name = "Will";
+        String phoneNumber = "302-293-1456";
+
+        phoneBook.add(name, phoneNumber);
+
+        String actual = phoneBook.reverseLookup(phoneNumber);
+        Assert.assertEquals(name, actual);
+    }
+
 }
